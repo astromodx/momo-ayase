cdcc71aa25b1d5dc2465aa0e4551286f5b258f66
astromodx <evellinhilario@gmail.com>
2024-12-14 12:46:44 -0300

    First commit

---
+++ settings/lib/menus.js
@@ -0,0 +1,548 @@
+const menu = (prefix, NomeDoBot, Emoji, sender, NickDono, packname) => {
+  
+  return `╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🦋𝐃𝐈𝐕𝐄𝐑𝐒𝐎𝐒-𝐌𝐄𝐍𝐔𝐒🦋⃟⃟き
+┃│${Emoji} » ${prefix}Menudono
+┃│${Emoji} » ${prefix}Menuadm
+┃│${Emoji} » ${prefix}Alteradores
+┃│${Emoji} » ${prefix}Menupremium
+┃│${Emoji} » ${prefix}Menurpg
+┃│${Emoji} » ${prefix}Menuanime
+┃│${Emoji} » ${prefix}Menulogos
+┃│${Emoji} » ${prefix}Brincadeiras
+┃│${Emoji} » ${prefix}Download
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🌸𝐂𝐌𝐃𝐒-𝐃𝐄-𝐌𝐄𝐌𝐁𝐑𝐎🌸⃟⃟き
+┃│${Emoji} » ${prefix}Bug (Questione/Erros) 
+┃│${Emoji} » ${prefix}Infodono (Info do dono)
+┃│${Emoji} » ${prefix}Comprarbot (informações)
+┃│${Emoji} » ${prefix}Sugestao (Dica/Sugestão)
+┃│${Emoji} » ${prefix}Avalie (O quanto é bom) 
+┃│${Emoji} » ${prefix}Infocmd (Comando) 
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🌼𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐕𝐎𝐒🌼⃟⃟き
+┃│${Emoji} » ${prefix}Ping (Velocidade/Up) 
+┃│${Emoji} » ${prefix}Atividade (do Grupo)
+┃│${Emoji} » ${prefix}Rankativo (do Grupo)
+┃│${Emoji} » ${prefix}Checkativo (@Marcar)
+┃│${Emoji} » ${prefix}Ranklevel (do Grupo) 
+┃│${Emoji} » ${prefix}Dados (dos Grupos)
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🌵𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐒🌵⃟き
+┃│${Emoji} » ${prefix}Play (Nome)
+┃│${Emoji} » ${prefix}Playvid (Nome)
+┃│${Emoji} » ${prefix}Tiktok (Link) 
+┃│${Emoji} » ${prefix}Tiktokaudio (Link)
+┃│${Emoji} » ${prefix}Instagram (Link) 
+┃│${Emoji} » ${prefix}Instaaudio (Link) 
+┃│${Emoji} » ${prefix}Instareels (Link)
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🍁𝐅𝐈𝐆𝐔𝐑𝐈𝐍𝐇𝐀𝐒🍁⃟き
+┃│${Emoji} » ${prefix}Attp (Seu texto)
+┃│${Emoji} » ${prefix}F (Marcar-foto)
+┃│${Emoji} » ${prefix}Sticker (Marcar-foto)
+┃│${Emoji} » ${prefix}Toimg (Marcar-sticker)
+┃│${Emoji} » ${prefix}Roubar (Texto/Texto)
+┃│${Emoji} » ${prefix}Rgtake (Texto|Texto)
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+};
+
+exports.menu = menu;
+
+const adms = (prefix, NomeDoBot, Emoji, sender, NickDono, packname) => { 
+
+return `╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🪼𝐌𝐄𝐍𝐔 - 𝐀𝐃𝐌𝐈𝐍𝐒🪼⃟⃟き
+┃│${Emoji} » ${prefix}Antiimg (1/0)
+┃│${Emoji} » ${prefix}Antivideo (1/0)
+┃│${Emoji} » ${prefix}Antiaudio (1/0)
+┃│${Emoji} » ${prefix}Antisticker (1/0)
+┃│${Emoji} » ${prefix}Antiloc (1/0)
+┃│${Emoji} » ${prefix}Anticontato (1/0)
+┃│${Emoji} » ${prefix}Antidoc (1/0)
+┃│${Emoji} » ${prefix}Antilinkgp (1/0)
+┃│${Emoji} » ${prefix}Antilinkhard (1/0)
+┃│${Emoji} » ${prefix}Antifake (1/0)
+┃│${Emoji} » ${prefix}Antinotas (1/0)
+┃│${Emoji} » ${prefix}Anticatalogo (1/0)
+┃│${Emoji} » ${prefix}Antipalavrao (1/0)
+┃│${Emoji} » ${prefix}Limitecaracteres (1/0)
+┃│${Emoji} » ${prefix}Bemvindo (1/0)
+┃│${Emoji} » ${prefix}Bemvindo2 (1/0)
+┃│${Emoji} » ${prefix}Simih (1/0)
+┃│${Emoji} » ${prefix}Simih2 (1/0)
+┃│${Emoji} » ${prefix}Autosticker (1/0)
+┃│${Emoji} » ${prefix}Autorepo (1/0)
+┃│${Emoji} » ${prefix}Leveling (1/0)
+┃│${Emoji} » ${prefix}Modonsfw (1/0)
+┃│${Emoji} » ${prefix}Odelete (1/0)
+┃│${Emoji} » ${prefix}x9visuunica (1/0)
+┃│${Emoji} » ${prefix}x9 (1/0)
+┃│${Emoji} » ${prefix}Limitecomandos
+┃│${Emoji} » ${prefix}Tempocmd (segundos)
+┃│${Emoji} » ${prefix}Legenda_imagem (Texto)
+┃│${Emoji} » ${prefix}Legenda_video (Texto)
+┃│${Emoji} » ${prefix}Legenda_estrangeiro
+┃│${Emoji} » ${prefix}Legendabv (Texto)
+┃│${Emoji} » ${prefix}Legendasaiu (Texto)
+┃│${Emoji} » ${prefix}Legendabv2 (Texto)
+┃│${Emoji} » ${prefix}Legendasaiu2 (Texto)
+┃│${Emoji} » ${prefix}So_adm
+┃│${Emoji} » ${prefix}Requestgp -list
+┃│${Emoji} » ${prefix}Requestgp -a numero
+┃│${Emoji} » ${prefix}Requestgp -r numero
+┃│${Emoji} » ${prefix}Fechar-gp
+┃│${Emoji} » ${prefix}Listanegra (Número)
+┃│${Emoji} » ${prefix}Tirardalista (Número)
+┃│${Emoji} » ${prefix}ListanegraG (Número)
+┃│${Emoji} » ${prefix}TirardalistaG (Número)
+┃│${Emoji} » ${prefix}Multiprefixo (1/0)
+┃│${Emoji} » ${prefix}Add_prefixo
+┃│${Emoji} » ${prefix}Tirar_prefixo
+┃│${Emoji} » ${prefix}Banghost
+┃│${Emoji} » ${prefix}Mute (@mencionar)
+┃│${Emoji} » ${prefix}Desmute (@mencionar)
+┃│${Emoji} » ${prefix}Add 5511.
+┃│${Emoji} » ${prefix}Reviver (Responder)
+┃│${Emoji} » ${prefix}Kick [@] (Para-remover) 
+┃│${Emoji} » ${prefix}Ban (Responder-mensagem)
+┃│${Emoji} » ${prefix}Promover [@] (Ser-admin)
+┃│${Emoji} » ${prefix}Rebaixar [@] (Rebaixar)
+┃│${Emoji} » ${prefix}Changegroup (all/adms)
+┃│${Emoji} » ${prefix}Rmphotogp (Remover)
+┃│${Emoji} » ${prefix}Ephemeral
+┃│${Emoji} » ${prefix}Descgp (Texto)
+┃│${Emoji} » ${prefix}Nomegp (Nome)
+┃│${Emoji} » ${prefix}Totag (Mencionar algo)
+┃│${Emoji} » ${prefix}Grupo (f/a)
+┃│${Emoji} » ${prefix}Status
+┃│${Emoji} » ${prefix}Limpar (texto-invisível-gp)
+┃│${Emoji} » ${prefix}Atividades (DO-GRUPO)
+┃│${Emoji} » ${prefix}Linkgp
+┃│${Emoji} » ${prefix}Grupoinfo
+┃│${Emoji} » ${prefix}Blockcmdgp (cmd)
+┃│${Emoji} » ${prefix}Unblockcmdgp (cmd)
+┃│${Emoji} » ${prefix}Listbcmdgp
+┃│${Emoji} » ${prefix}Hidetag (txt) (marcação)
+┃│${Emoji} » ${prefix}Marcar (marca tds do gp)
+┃│${Emoji} » ${prefix}Marcar2 (Marca-tds-wa.me)
+┃│${Emoji} » ${prefix}Anagrama (1/0)
+┃│${Emoji} » ${prefix}Antipalavra (1/0)
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+};
+
+exports.adms = adms;
+
+const menudono = (prefix, NomeDoBot, Emoji, sender, NickDono, packname) => {	
+
+return `​╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🫅🏻𝐌𝐄𝐍𝐔 𝐏𝐑𝐎𝐏𝐑𝐈𝐄𝐓𝐀𝐑𝐈𝐎🫅🏻⃟き
+┃│${Emoji} » ${prefix}Bangp
+┃│${Emoji} » ${prefix}Unbangp
+┃│${Emoji} » ${prefix}Fotomenu 
+┃│${Emoji} » ${prefix}Cmdpremlist
+┃│${Emoji} » ${prefix}Addcmdprem
+┃│${Emoji} » ${prefix}Delcmdprem
+┃│${Emoji} » ${prefix}Fundobemvindo 
+┃│${Emoji} » ${prefix}Fundosaiu 
+┃│${Emoji} » ${prefix}Serpremium
+┃│${Emoji} » ${prefix}Listagp
+┃│${Emoji} » ${prefix}Antipalavrão 
+┃│${Emoji} » ${prefix}Antiligar 
+┃│${Emoji} » ${prefix}Modoaluguel 
+┃│${Emoji} » ${prefix}Fazertm
+┃│${Emoji} » ${prefix}Rgtm
+┃│${Emoji} » ${prefix}Tirardatm
+┃│${Emoji} » ${prefix}Listatm
+┃│${Emoji} » ${prefix}Infocmd_add 
+┃│${Emoji} » ${prefix}Infocmd_del 
+┃│${Emoji} » ${prefix}Visualizarmsg
+┃│${Emoji} » ${prefix}Botoff
+┃│${Emoji} » ${prefix}Boton 
+┃│${Emoji} » ${prefix}Verificado-global
+┃│${Emoji} » ${prefix}Audio-menu
+┃│${Emoji} » ${prefix}Addpalavra
+┃│${Emoji} » ${prefix}Delpalavra
+┃│${Emoji} » ${prefix}Ativo
+┃│${Emoji} » ${prefix}Rmpalavra_forca 
+┃│${Emoji} » ${prefix}Addpalavras_forca 
+┃│${Emoji} » ${prefix}Listbcmdglobal
+┃│${Emoji} » ${prefix}Blockcmdg
+┃│${Emoji} » ${prefix}Unblockcmdg
+┃│${Emoji} » ${prefix}Buscarsc
+┃│${Emoji} » ${prefix}Addpix
+┃│${Emoji} » ${prefix}Setpix 
+┃│${Emoji} » ${prefix}Delpix 
+┃│${Emoji} » ${prefix}Zerarsc 
+┃│${Emoji} » ${prefix}Gerargf
+┃│${Emoji} » ${prefix}Delgf 
+┃│${Emoji} » ${prefix}alugargp
+┃│${Emoji} » ${prefix}removeraluguel
+┃│${Emoji} » ${prefix}Cortesia24
+┃│${Emoji} » ${prefix}Lista-aluguel
+┃│${Emoji} » ${prefix}Codelistgp
+┃│${Emoji} » ${prefix}Gerarcodegp
+┃│${Emoji} » ${prefix}Delcodegp
+┃│${Emoji} » ${prefix}Ausente
+┃│${Emoji} » ${prefix}Delpremium
+┃│${Emoji} » ${prefix}Addpremium
+┃│${Emoji} » ${prefix}Privphotobot 
+┃│${Emoji} » ${prefix}Privaddgroup 
+┃│${Emoji} » ${prefix}Addlevel
+┃│${Emoji} » ${prefix}Tirarlevel
+┃│${Emoji} » ${prefix}Addxp
+┃│${Emoji} » ${prefix}Tirarxp
+┃│${Emoji} » ${prefix}Blocklevellist
+┃│${Emoji} » ${prefix}Blocklevel
+┃│${Emoji} » ${prefix}Unblocklevel
+┃│${Emoji} » ${prefix}Rmlevel
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+};
+
+exports.menudono = menudono;
+
+const menulogos = (prefix, sender, NomeDoBot, Emoji, NickDono, packname) => {
+  
+return `╭═══════════════════ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🌄𝐌𝐄𝐍𝐔 - 𝐋𝐎𝐆𝐎𝐒🌄⃟⃟き
+┃│${Emoji} » ${prefix}Efeitoneon (Texto)
+┃│${Emoji} » ${prefix}Cemiterio (Texto)
+┃│${Emoji} » ${prefix}Narutologo (Texto)
+┃│${Emoji} » ${prefix}Narutologo2 (Texto)
+┃│${Emoji} » ${prefix}Fire (Texto)
+┃│${Emoji} » ${prefix}Romantic (Texto)
+┃│${Emoji} » ${prefix}Smoke (Texto)
+┃│${Emoji} » ${prefix}Papel (Texto)
+┃│${Emoji} » ${prefix}Lovemsg (Texto)
+┃│${Emoji} » ${prefix}Coffecup (Texto)
+┃│${Emoji} » ${prefix}Cup (Texto)
+┃│${Emoji} » ${prefix}Florwooden (Texto)
+┃│${Emoji} » ${prefix}Lobometal (Texto)
+┃│${Emoji} » ${prefix}Harryp (Texto)
+┃│${Emoji} » ${prefix}Txtborboleta (Texto)
+┃│${Emoji} » ${prefix}Madeira (Texto)
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+};
+
+exports.menulogos = menulogos;
+
+const alteradores = (prefix, sender, NomeDoBot, Emoji, NickDono, packname) => {
+
+return`╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🫧𝐀𝐋𝐓𝐄𝐑𝐀𝐑-𝐕𝐈𝐃𝐄𝐎🫧⃟き
+┃│${Emoji} » ${prefix}Videolento (marca)
+┃│${Emoji} » ${prefix}Videorapido (marca)
+┃│${Emoji} » ${prefix}Videocontrario (marca)
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🫧𝐀𝐋𝐓𝐄𝐑𝐀𝐑-𝐀𝐔𝐃𝐈𝐎🫧⃟き
+┃│${Emoji} » ${prefix}Audiolento (marca)
+┃│${Emoji} » ${prefix}Audiorapido (marca)
+┃│${Emoji} » ${prefix}Grave (marca)
+┃│${Emoji} » ${prefix}Grave2 (marca)
+┃│${Emoji} » ${prefix}Esquilo (marca)
+┃│${Emoji} » ${prefix}Estourar (marca)
+┃│${Emoji} » ${prefix}Bass (marca)
+┃│${Emoji} » ${prefix}Bass2 (marca)
+┃│${Emoji} » ${prefix}Vozmenino (marca)
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+};
+
+exports.alteradores = alteradores;
+
+const menuprem = (prefix, sender, NomeDoBot, Emoji, NickDono, packname) => { 
+
+return `╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🌟𝐌𝐄𝐍𝐔-𝐏𝐑𝐄𝐌𝐈𝐔𝐌🌟⃟き
+┃│${Emoji} » ${prefix}Destrava
+┃│${Emoji} » ${prefix}Gpt (pergunte algo)
+┃│${Emoji} » ${prefix}Listaddd (Número)
+┃│${Emoji} » ${prefix}Gerarcpf
+┃│${Emoji} » ${prefix}Premiumlist
+┃│${Emoji} » ${prefix}Tinyurl (Link)
+┃│${Emoji} » ${prefix}Cuttly (Link)
+┃│${Emoji} » ${prefix}Bitly (Link)
+┃│${Emoji} » ${prefix}Celular (Nome)
+┃│${Emoji} » ${prefix}Scep (Número)
+┃│${Emoji} » ${prefix}Cotacao (Moeda)
+┃│${Emoji} » ${prefix}Dinextenso (Valor)
+┃│${Emoji} » ${prefix}Sip (Número)
+┃│${Emoji} » ${prefix}Validarcnpj (número)
+┃│${Emoji} » ${prefix}Igstalk (@usuário)
+┃│${Emoji} » ${prefix}Validarcpf (número)
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+};
+
+exports.menuprem = menuprem;
+
+const brincadeiras = (prefix, sender, NomeDoBot, Emoji, NickDono, packname) => {
+
+return `​╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🧸𝐁𝐑𝐈𝐍𝐂𝐀𝐃𝐄𝐈𝐑𝐀𝐒🧸⃟⃟き
+┃│${Emoji} » ${prefix}Jogodavelha (@Marcar) 
+┃│${Emoji} » ${prefix}Anagrama (1/0)
+┃│${Emoji} » ${prefix}Gartic (1/0)
+┃│${Emoji} » ${prefix}Whatmusic (1/0)
+┃│${Emoji} » ${prefix}Quizanimal (1/0)
+┃│${Emoji} » ${prefix}Enigma (1/0)
+┃│${Emoji} » ${prefix}R-forca (Letra)
+┃│${Emoji} » ${prefix}Resetforca (Resetar)
+┃│${Emoji} » ${prefix}Jogodaforca (Iniciar)
+┃│${Emoji} » ${prefix}Vab (Você prefere?)
+┃│${Emoji} » ${prefix}Eununca (Eu nunca, eu já)
+┃│${Emoji} » ${prefix}Ppt (Pedra/Papel/Tesoura) 
+┃│${Emoji} » ${prefix}Cassino
+┃│${Emoji} » ${prefix}Mina (coordenada(s))
+┃│${Emoji} » ${prefix}Minado (dificuldade)
+┃│${Emoji} » ${prefix}Minareset (resetar)
+┃│${Emoji} » ${prefix}Mineshelp (info)
+┃│${Emoji} » ${prefix}Minatips (dicas)
+┃│${Emoji} » ${prefix}Akinator (iniciar jogo)
+┃│${Emoji} » ${prefix}Resetaki (resetar akinator)
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🏓𝐁𝐑𝐈𝐍𝐂𝐀𝐃𝐄𝐈𝐑𝐀𝐒🏓⃟⃟き
+┃│${Emoji} » ${prefix}Gay (marca (@))
+┃│${Emoji} » ${prefix}Feio (marca (@))
+┃│${Emoji} » ${prefix}Corno (marca (@))
+┃│${Emoji} » ${prefix}Vesgo (marca (@))
+┃│${Emoji} » ${prefix}Bebado (marca (@))
+┃│${Emoji} » ${prefix}Gostoso (marca (@))
+┃│${Emoji} » ${prefix}Gostosa (marca (@))
+┃│${Emoji} » ${prefix}Beijo (marca (@))
+┃│${Emoji} » ${prefix}Matar (marca (@))
+┃│${Emoji} » ${prefix}Tapa (marca (@))
+┃│${Emoji} » ${prefix}Chute (marca (@))
+┃│${Emoji} » ${prefix}Dogolpe (marca (@))   
+┃│${Emoji} » ${prefix}Nazista (marca (@))
+┃│${Emoji} » ${prefix}Chance (fale algo) 
+┃│${Emoji} » ${prefix}Surubao (Quantidade) 
+┃│${Emoji} » ${prefix}Casal (Casal do grupo)
+┃│${Emoji} » ${prefix}Quando (Perguntar)
+┃│${Emoji} » ${prefix}Mencionar (fale algo)
+┃│${Emoji} » ${prefix}Death (Nome)
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🫅🏻𝐓𝐎𝐏 𝟓 - 𝐑𝐀𝐍𝐊🫅🏻⃟⃟き
+┃│${Emoji} » ${prefix}Rankgay (5 gays)
+┃│${Emoji} » ${prefix}Rankgado (5 gados)
+┃│${Emoji} » ${prefix}Rankcorno (5 cornos)
+┃│${Emoji} » ${prefix}Rankgostoso (5 gostosos)
+┃│${Emoji} » ${prefix}Rankgostosa (5 gostosas)
+┃│${Emoji} » ${prefix}Ranknazista (5 nazistas)
+┃│${Emoji} » ${prefix}Rankotakus (5 otakus)
+┃│${Emoji} » ${prefix}Rankpau (5 pauzudos)
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+};
+
+exports.brincadeiras = brincadeiras;
+
+const rpgmenu = (prefix, sender, NomeDoBot, Emoji, NickDono, packname) => {
+
+return `╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟⛩️𝐂𝐈𝐓𝐘-𝐎𝐅𝐈𝐂𝐈𝐀𝐋⛩️⃟き
+┃│${Emoji} » ${prefix}Lojadecavalos
+┃│${Emoji} » ${prefix}Estabulo
+┃│${Emoji} » ${prefix}Lojadegalos
+┃│${Emoji} » ${prefix}Galos
+┃│${Emoji} » ${prefix}Galinheiro
+┃│${Emoji} » ${prefix}Cruzargg
+┃│${Emoji} » ${prefix}Dadoapostado
+┃│${Emoji} » ${prefix}Caracoroa
+┃│${Emoji} » ${prefix}Modomomo
+┃│${Emoji} » ${prefix}Rgcity
+┃│${Emoji} » ${prefix}Rpglistgp
+┃│${Emoji} » ${prefix}Minhacity
+┃│${Emoji} » ${prefix}Minhacarteira
+┃│${Emoji} » ${prefix}Rankcity
+┃│${Emoji} » ${prefix}Codelist
+┃│${Emoji} » ${prefix}Resgatargf
+┃│${Emoji} » ${prefix}Fazerpix
+┃│${Emoji} » ${prefix}Chavepix
+┃│${Emoji} » ${prefix}Meupix
+┃│${Emoji} » ${prefix}Cassino 
+┃│${Emoji} » ${prefix}1xbets
+┃│${Emoji} » ${prefix}Assaltar
+┃│${Emoji} » ${prefix}Minerar
+┃│${Emoji} » ${prefix}Retirar
+┃│${Emoji} » ${prefix}Trocarbanco 
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`
+}
+
+exports.rpgmenu = rpgmenu;
+
+const efeitos = (prefix, sender, NomeDoBot, Emoji, NickDono, packname) => {
+
+return `​╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🎭𝐌𝐄𝐍𝐔-𝐃𝐄-𝐄𝐅𝐄𝐈𝐓𝐎𝐒🎭⃟⃟き
+┃│${Emoji} » ${prefix}Comunismo (marcar)-(img)
+┃│${Emoji} » ${prefix}Bolsonaro (marcar)-(img)
+┃│${Emoji} » ${prefix}Bnw (marcar)-(img)
+┃│${Emoji} » ${prefix}Beautiful (marcar)-(img)
+┃│${Emoji} » ${prefix}Blur (marcar)-(img)
+┃│${Emoji} » ${prefix}Affect (marcar)-(img)
+┃│${Emoji} » ${prefix}Del (marcar)-(img)
+┃│${Emoji} » ${prefix}Circle (marcar)-(img)
+┃│${Emoji} » ${prefix}Beautiful (marcar)-(img)
+┃│${Emoji} » ${prefix}Dither (marcar)-(img)
+┃│${Emoji} » ${prefix}Facepalm (marcar)-(img)
+┃│${Emoji} » ${prefix}Invert (marcar)-(img)
+┃│${Emoji} » ${prefix}Lgbt (marcar)-(img)
+┃│${Emoji} » ${prefix}Magik (marcar)-(img)
+┃│${Emoji} » ${prefix}Rotate (marcar)-(img)
+┃│${Emoji} » ${prefix}Rip (marcar)-(img)
+┃│${Emoji} » ${prefix}Jail (marcar)-(img)
+┃│${Emoji} » ${prefix}Trash (marcar)-(img)
+┃│${Emoji} » ${prefix}Pixelate (marcar)-(img)
+┃│${Emoji} » ${prefix}Sepia (marcar)-(img)
+┃│${Emoji} » ${prefix}Wanted (marcar)-(img) 
+┃│${Emoji} » ${prefix}Wasted (marcar)-(img) 
+┃│${Emoji} » ${prefix}Animeia (marcar)-(img)
+┃│${Emoji} » ${prefix}Zombieia (marcar)-(img)
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+};
+
+exports.efeitos = efeitos;
+
+const animes = (prefix, sender, NomeDoBot, Emoji, NickDono, packname) => {
+
+return `​╭━━━━━━━━━━━━━━━━━━━ ⪩
+╰╮き👑⃟𝐈𝐍𝐅𝐎-𝐁𝐎𝐓/𝐔𝐒𝐄𝐑👑⃟き
+╭┤🐼 » Usuário: *@${sender.split("@")[0]}*
+┃│💡 » Minha Versão: *5.0*
+┃│🫅🏻 » Dono: *${NickDono}*
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🎨𝐀𝐍𝐈𝐌𝐄𝐒-𝐄𝐃𝐈𝐓𝐒🎨⃟き
+┃│${Emoji} » ${prefix}Editanime -random
+┃│${Emoji} » ${prefix}Editanime -bleach
+┃│${Emoji} » ${prefix}Editanime -chainsaw
+┃│${Emoji} » ${prefix}Editanime -dragonball
+┃│${Emoji} » ${prefix}Editanime -kimetsu
+┃│${Emoji} » ${prefix}Editanime -jujutsu
+┃│${Emoji} » ${prefix}Editanime -naruto
+┃╰━━ ⪨
+╰╮
+╭┤き⃟🪀𝐀𝐍𝐈𝐌𝐄𝐒-𝐈𝐌𝐀𝐆𝐄𝐌🪀⃟⃟き
+┃│${Emoji} » ${prefix}Animeimage -cosplay
+┃│${Emoji} » ${prefix}Animeimage -waifu
+┃│${Emoji} » ${prefix}Animeimage -loli
+┃│${Emoji} » ${prefix}Animeimage -shota
+┃│${Emoji} » ${prefix}Animeimage -shinomiya
+┃│${Emoji} » ${prefix}Animeimage -yotsuba
+┃│${Emoji} » ${prefix}Animeimage -yumeko
+┃│${Emoji} » ${prefix}Animeimage -tejina
+┃│${Emoji} » ${prefix}Animeimage -chiho
+┃│${Emoji} » ${prefix}Animeimage -kaori
+┃│${Emoji} » ${prefix}Animeimage -boruto
+┃│${Emoji} » ${prefix}Animeimage -shizuka
+┃│${Emoji} » ${prefix}Animeimage -kaga
+┃│${Emoji} » ${prefix}Animeimage -kotori
+┃│${Emoji} » ${prefix}Animeimage -mikasa
+┃│${Emoji} » ${prefix}Animeimage -akiyama
+┃│${Emoji} » ${prefix}Animeimage -gremory
+┃│${Emoji} » ${prefix}Animeimage -izuku
+┃│${Emoji} » ${prefix}Animeimage -shina
+┃│${Emoji} » ${prefix}Animeimage -shinka
+┃│${Emoji} » ${prefix}Animeimage -yuri
+┃│${Emoji} » ${prefix}Animeimage -eba
+┃│${Emoji} » ${prefix}Animeimage -erza
+┃│${Emoji} » ${prefix}Animeimage -elaina
+┃│${Emoji} » ${prefix}Animeimage -hinata
+┃│${Emoji} » ${prefix}Animeimage -naruto
+┃│${Emoji} » ${prefix}Animeimage -minato
+┃│${Emoji} » ${prefix}Animeimage -sagari
+┃│${Emoji} » ${prefix}Animeimage -nezuko
+┃│${Emoji} » ${prefix}Animeimage -rize
+┃│${Emoji} » ${prefix}Animeimage -anna
+┃│${Emoji} » ${prefix}Animeimage -deidara
+┃│${Emoji} » ${prefix}Animeimage -asuna
+┃│${Emoji} » ${prefix}Animeimage -ayuzawa
+┃│${Emoji} » ${prefix}Animeimage -emilia
+┃│${Emoji} » ${prefix}Animeimage -chitoge
+┃│${Emoji} » ${prefix}Animeimage -hestia
+┃│${Emoji} » ${prefix}Animeimage -inori
+┃│${Emoji} » ${prefix}Animeimage -itachi
+┃│${Emoji} » ${prefix}Animeimage -madara
+┃│${Emoji} » ${prefix}Animeimage -sakura
+┃│${Emoji} » ${prefix}Animeimage -sasuke
+┃│${Emoji} » ${prefix}Animeimage -tsunade
+┃│${Emoji} » ${prefix}Animeimage -onepiece
+┃│${Emoji} » ${prefix}Animeimage -mobil
+┃│${Emoji} » ${prefix}Animeimage -montor
+┃│${Emoji} » ${prefix}Animeimage -keneki
+┃│${Emoji} » ${prefix}Animeimage -megumin
+┃│${Emoji} » ${prefix}Animeimage -toukachan
+┃╰━━ ⪨
+╰━━━━━━━━━━━━━━━━━━━ ⪨`;
+
+};
+
+exports.animes = animes;
